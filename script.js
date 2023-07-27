d65a700ffc02d70348c6477152e3c93622926f34
vipulmalviya <vipulmalviya9987@gmail.com>
2023-07-27 12:39:18 +0530

    change

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-<<<<<<< HEAD
-=======
-function loco(){
-	gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+function loco() {
+    gsap.registerPlugin(ScrollTrigger);
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("body"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-// tell ScrollTrigger to use these proxy methods for the "body" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("body", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
-});
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("body"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
 
+    // tell ScrollTrigger to use these proxy methods for the "body" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("body", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
+    });
 
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
 
 }
 loco()
@@ -39,7 +38,7 @@ loco()
 
 
 
->>>>>>> 363ae87 (scroll smooth)
+
 gsap.to(".slidingText h1", {
     x: -500,
     duration: 3,
@@ -104,27 +103,27 @@ gsap.from(".normalButton", {
 gsap.from(".icone img", {
     opacity: 0,
     scrollTrigger: {
-      trigger: ".icone img",
-      scroller: "body",
-      // markers:true,
-      start: "top 80%"
+        trigger: ".icone img",
+        scroller: "body",
+        // markers:true,
+        start: "top 80%"
     },
     y: 20,
     stagger: {
-      amount: 2
+        amount: 2
     }
-  
-  })
-  gsap.from(".icone2 img", {
+
+})
+gsap.from(".icone2 img", {
     opacity: 0,
     scrollTrigger: {
-      trigger: ".icone2 img",
-      scroller: "body",
-      // markers:true,
-      start: "top 80%"
+        trigger: ".icone2 img",
+        scroller: "body",
+        // markers:true,
+        start: "top 80%"
     },
     y: 20,
     stagger: {
-      amount: 1
+        amount: 1
     }
-  })
+})
